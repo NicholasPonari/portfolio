f0afa84e3216adf588bc76ef8aa0095ec40abf4e
NicholasPonari <41348835+NicholasPonari@users.noreply.github.com>
2022-10-12 22:54:59 -0400

    Update and Polishing

--- Rust Projects - Rust Practice/collections_enums_practice/target/debug/deps/g_collections_enums-2c18d8d50e71e997.d
+++ Rust Projects - Rust Practice/collections_enums_practice/target/debug/deps/g_collections_enums-2c18d8d50e71e997.d
@@ -1,5 +1,5 @@
-c:\Users\Nick_\Desktop\ultimate_rust_crash_course-main\exercise\g_collections_enums\target\debug\deps\g_collections_enums-2c18d8d50e71e997.rmeta: src\main.rs
+c:\Users\Nick_\Desktop\Coding Projects\portfolio\Rust Projects - Rust Practice\collections_enums_practice\target\debug\deps\g_collections_enums-2c18d8d50e71e997.rmeta: src\main.rs
 
-c:\Users\Nick_\Desktop\ultimate_rust_crash_course-main\exercise\g_collections_enums\target\debug\deps\g_collections_enums-2c18d8d50e71e997.d: src\main.rs
+c:\Users\Nick_\Desktop\Coding Projects\portfolio\Rust Projects - Rust Practice\collections_enums_practice\target\debug\deps\g_collections_enums-2c18d8d50e71e997.d: src\main.rs
 
 src\main.rs:
